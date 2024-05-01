905a6ad0109341c96d4537db590c087102c6aab2
younesfdj <ferradjiyounes69@gmail.com>
2024-05-01 15:55:59 +0100

    Untrack files in .gitignore

--- dist/routes/test.router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const test_controller_1 = require("../controllers/test.controller");
-const testRouter = (0, express_1.Router)();
-testRouter.get("/test", test_controller_1.test);
-exports.default = testRouter;
--- dist/services/test.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.testService = void 0;
-const __1 = require("..");
-const testService = async () => {
-    const test = await __1.prismaClient.testtable.findMany();
-    return test;
-};
-exports.testService = testService;
