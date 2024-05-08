a3f06c8089b37825247e8c3774313fcf3a5a394b
younesfdj <ferradjiyounes69@gmail.com>
2024-05-08 16:03:44 +0100

    update: switched to postgres

--- prisma/migrations/20240505140812_uniquify_user_email/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[UserEmail]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `User_UserEmail_key` ON `User`(`UserEmail`);
